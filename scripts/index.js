6082235f526c1ed6eae6bb401153be3b63e88145
X <bernardtech.co1@gmail.com>
2026-04-04 10:17:49 +0100

    saved work before pull

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,14 @@
-const view = document.querySelector('.view');
-const orderBtn = document.querySelector('.order');
+// const view = document.querySelector('.view');
+// const orderBtn = document.querySelector('.order');
 
-orderBtn.addEventListener('click', () => {
-    window.location.href = 'menu.html';
-});
+// orderBtn.addEventListener('click', () => {
+//     window.location.href = 'menu.html';
+// });
 
-view.addEventListener('click', () => {
-    window.location.href = 'restaurant.html';
-});
+// view.addEventListener('click', () => {
+//     window.location.href = 'restaurant.html';
+// });
+
+
+const colorchange = document.getElementsByClassName("money")
+colorchange.style.color = "green";
